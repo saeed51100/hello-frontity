4b022a4a3fa45d88c50b59239a3e9ad398205187
saeed <saeedblack77@gmail.com>
2023-03-14 19:00:24 +0330

    khooneh 1401/12/23 16:59 SEO and head tags

--- packages/my-first-theme/src/components/post.js
+++ packages/my-first-theme/src/components/post.js
@@ -1,8 +1,8 @@
 import React from "react"
-import {connect, styled} from "frontity"
+import { connect, styled, Head } from "frontity"
 import dayjs from "dayjs"
 
-const Post = ({state, libraries}) => {
+const Post = ({ state, libraries }) => {
     const data = state.source.get(state.router.link)
     const post = state.source[data.type][data.id]
     const author = state.source.author[post.author]
@@ -12,6 +12,10 @@ const Post = ({state, libraries}) => {
 
     return (
         <div>
+            <Head>
+                <title>{post.title.rendered}</title>
+                <meta name="description" content={post.excerpt.rendered} />
+            </Head>
             <h2>{post.title.rendered}</h2>
             <PostInfo>
                 <p>
@@ -23,7 +27,7 @@ const Post = ({state, libraries}) => {
                     {author.name}
                 </p>
             </PostInfo>
-            <Html2React html={post.content.rendered}/>
+            <Html2React html={post.content.rendered} />
         </div>
     )
 }
